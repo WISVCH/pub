6f86deebf36789c1448b21827d23e57fcccd8f02
Joep de Jong <j.dejong-16@student.tudelft.nl>
2021-09-10 17:59:59 +0200

    Update event text

--- components/EventsIntro.jsx
+++ components/EventsIntro.jsx
@@ -21,7 +21,8 @@ export const EventsIntro = () => {
     return (
         <section className={styles.container}>
           <h1>Events in the <span>/Pub</span></h1>
-          <p>The /Pub is open on wednesday and thursday from 16:00 to 19:00. It is possible to organize events in the /Pub. This can only be done outside opening hours and only for special occasions.</p>
+          <p>The /Pub is open for drinks on wednesday and thursday, but that does not mean that the /Pub is not available the rest of the week. Besides the planned events and the regular opening hours (wednesday and thursday 16:00 - 19:00), it is also possible to reserve the /Pub, for example for your graduation drinks or a lunch. To make a reservation an email can be sent to pub-ewi@tudelft.nl (In the near future it will also be possible to make a reservation via this website). This email should include the following: Information about the organizer, start and end time of the event, expected amount of visitors and a short description about the event. Note that an event with 50 or more visitors, or an event with an end time later than 19:00 should be communicated at least 2 weeks before the event.</p>
+          <p>Below you can find the calendar with all the planned events.</p>
           <h2>Schedule</h2>
           <FullCalendar
             plugins={[ dayGridPlugin, googleCalendarPlugin ]}
