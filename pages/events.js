27ca747beced9f912a74064df2c96af162aa24ca
Joep de Jong <j.dejong-16@student.tudelft.nl>
2021-09-10 18:07:31 +0200

    Add icons

--- pages/events.js
+++ pages/events.js
@@ -10,7 +10,18 @@ export default function Events() {
     <div className="container">
       <Head>
         <title>Events - /Pub</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png"/>
+        <link rel="manifest" href="/icons/site.webmanifest"/>
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#8eac91"/>
+        <link rel="shortcut icon" href="/icons/favicon.ico"/>
+        <meta name="apple-mobile-web-app-title" content="/Pub"/>
+        <meta name="application-name" content="/Pub"/>
+        <meta name="msapplication-TileColor" content="#8eac91"/>
+        <meta name="msapplication-config" content="/icons/browserconfig.xml"/>
+        <meta name="theme-color" content="#ffffff"/>
+        
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400&display=swap" rel="stylesheet"></link>
