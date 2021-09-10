a437ba4421cf02c7a9c9d1a7ddb3ef988c559bce
Joep de Jong <j.dejong-16@student.tudelft.nl>
2021-09-10 17:40:16 +0200

    Add founders

--- components/Board.jsx
+++ components/Board.jsx
@@ -1,6 +1,6 @@
 import styles from './Board.module.scss'
 
-export const Board = ({img, chairman, secretary, treasurer, commisisonerIntern = null, commisisonerOfInventory, commisisonerOfBarkeepers = null, commisisonerOfEvents = null, commisisonerOfMaintenance = null, qualitateQua}) => {
+export const Board = ({img, chairman, secretary, treasurer, commissioner = null, commissionerIntern = null, commissionerOfInventory, commissionerOfBarkeepers = null, commissionerOfEvents = null, commissionerOfMaintenance = null, qualitateQua}) => {
     return (
         <section className={styles.container}>
             <div>
@@ -15,12 +15,13 @@ export const Board = ({img, chairman, secretary, treasurer, commisisonerIntern =
                         <tr><th>Chairman</th><td>{chairman}</td></tr>
                         <tr><th>Secretary</th><td>{secretary}</td></tr>
                         <tr><th>Treasurer</th><td>{treasurer}</td></tr>
-                        { commisisonerIntern && <tr><th>Commisisoner Intern</th><td>{commisisonerIntern}</td></tr>}
-                        <tr><th>Commisisoner of Inventory</th><td>{commisisonerOfInventory}</td></tr>
-                        { commisisonerOfBarkeepers && <tr><th>Commisisoner of Barkeepers</th><td>{commisisonerOfBarkeepers}</td></tr>}
-                        { commisisonerOfEvents && <tr><th>Commisisoner of Events</th><td>{commisisonerOfEvents}</td></tr>}
-                        { commisisonerOfMaintenance && <tr><th>Commisisoner of Maintenance</th><td>{commisisonerOfMaintenance}</td></tr>}
-                        <tr><th>Qualitate Qua</th><td>{qualitateQua}</td></tr>
+                        { commissioner && <tr><th>Commissioner</th><td>{commissioner}</td></tr>}
+                        { commissionerIntern && <tr><th>Commissioner Intern</th><td>{commissionerIntern}</td></tr>}
+                        { commissionerOfInventory && <tr><th>Commissioner of Inventory</th><td>{commissionerOfInventory}</td></tr>}
+                        { commissionerOfBarkeepers && <tr><th>Commissioner of Barkeepers</th><td>{commissionerOfBarkeepers}</td></tr>}
+                        { commissionerOfEvents && <tr><th>Commissioner of Events</th><td>{commissionerOfEvents}</td></tr>}
+                        { commissionerOfMaintenance && <tr><th>Commissioner of Maintenance</th><td>{commissionerOfMaintenance}</td></tr>}
+                        { qualitateQua && <tr><th>Qualitate Qua</th><td>{qualitateQua}</td></tr>}
                     </tbody>
                 </table>
             </div>
--- components/Boards.jsx
+++ components/Boards.jsx
@@ -6,20 +6,19 @@ import { Button } from './Button'
 export const Boards = () => {
     const [activeBoard, setActiveBoard] = useState(2)
     return (
-        <section className={styles.container}>
+        <section className={styles.container} id="boards">
             <h2>Boards</h2>
             <div className={styles.buttonWrapper}>
-                {/* <Button onClick={() => setActiveBoard(3)}>Board 2</Button> */}
                 <Button onClick={() => setActiveBoard(3)} active={activeBoard == 3}>Board 3</Button>
                 <Button onClick={() => setActiveBoard(2)} active={activeBoard == 2}>Board 2</Button>
                 <Button onClick={() => setActiveBoard(1)} active={activeBoard == 1}>Board 1</Button>
-                {/* <Button onClick={() => setActiveBoard(0)} active={activeBoard == 0}>Founders</Button> */}
+                <Button onClick={() => setActiveBoard(0)} active={activeBoard == 0}>Founders</Button>
             </div>
             <div className={styles.boardWrapper}>
-                {activeBoard == 3 && <Board name="Board 3" img={"/images/board-3.jpeg"} chairman="Shirley Li " secretary="Amanda Krudde" treasurer="Lars van Tol" commisisonerOfInventory="Simon Molenkamp" commisisonerOfEvents="Bart Zuidema" commisisonerOfMaintenance="Daan Schat " qualitateQua="Niels Mateijsen &amp; Maxim Maxurovs"/>}
-                {activeBoard == 2 && <Board name="Board 2" img={"/images/board-2.jpeg"} chairman="Owen van Hooff" secretary="Maxim Maxurovs" treasurer="Niels Mateijsen" commisisonerOfInventory="Sam de Jong" commisisonerOfEvents="Joep de Jong" commisisonerOfMaintenance="Robbin Poll" qualitateQua="Frederiek Backers &amp; Joris van Breukelen"/>}
-                {activeBoard == 1 && <Board name="Board 1" img={"/images/board-1.jpg"} chairman="Bastiaan Bakker" secretary="Marcel Brouwers" treasurer="Frederiek Backers" commisisonerIntern="Joris van Breukelen" commisisonerOfInventory="Rona Roovers" commisisonerOfBarkeepers="Robbin Poll" qualitateQua="Louise Zwep &amp; Joos Vrijdag"/>}
-                {/* {activeBoard == 0 && <Board name="Founders" img={"/images/board-0.jpg"} chairman="Bastiaan Bakker" secretary="Marcel Brouwers" treasurer="Frederiek Backers" commisisonerIntern="Joris van Breukelen" commisisonerOfInventory="Rona Roovers" commisisonerOfBarkeepers="Robbin Poll" qualitateQua="Louise Zwep &amp; Joos Vrijdag"/>} */}
+                {activeBoard == 3 && <Board name="Board 3" img={"/images/board-3.jpeg"} chairman="Shirley Li " secretary="Amanda Krudde" treasurer="Lars van Tol" commissionerOfInventory="Simon Molenkamp" commissionerOfEvents="Bart Zuidema" commissionerOfMaintenance="Daan Schat " qualitateQua="Niels Mateijsen &amp; Maxim Maxurovs"/>}
+                {activeBoard == 2 && <Board name="Board 2" img={"/images/board-2.jpeg"} chairman="Owen van Hooff" secretary="Maxim Maxurovs" treasurer="Niels Mateijsen" commissionerOfInventory="Sam de Jong" commissionerOfEvents="Joep de Jong" commissionerOfMaintenance="Robbin Poll" qualitateQua="Frederiek Backers &amp; Joris van Breukelen"/>}
+                {activeBoard == 1 && <Board name="Board 1" img={"/images/board-1.jpg"} chairman="Bastiaan Bakker" secretary="Marcel Brouwers" treasurer="Frederiek Backers" commissionerIntern="Joris van Breukelen" commissionerOfInventory="Rona Roovers" commissionerOfBarkeepers="Robbin Poll" qualitateQua="Louise Zwep &amp; Joos Vrijdag"/>}
+                {activeBoard == 0 && <Board name="Founders" img={"/images/board-0.jpeg"} chairman="Rik Westdorp" secretary="Philip Groet" treasurer="Willem de Laat" commissioner="Yanna van der Vlugt"/>}
             </div>
         </section>
     )
