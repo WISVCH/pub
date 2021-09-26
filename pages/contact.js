715108f98654c4d90877900262060b7ea9cdd85a
Joep de Jong <j.dejong-16@student.tudelft.nl>
2021-09-26 11:58:23 +0200

    Move icons to root

--- pages/contact.js
+++ pages/contact.js
@@ -8,16 +8,16 @@ export default function Contact() {
     <div className="container">
       <Head>
         <title>Contact - /Pub</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png"/>
-        <link rel="manifest" href="/icons/site.webmanifest"/>
-        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#8eac91"/>
-        <link rel="shortcut icon" href="/icons/favicon.ico"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#8eac91"/>
+        <link rel="shortcut icon" href="/favicon.ico"/>
         <meta name="apple-mobile-web-app-title" content="/Pub"/>
         <meta name="application-name" content="/Pub"/>
         <meta name="msapplication-TileColor" content="#8eac91"/>
-        <meta name="msapplication-config" content="/icons/browserconfig.xml"/>
+        <meta name="msapplication-config" content="/browserconfig.xml"/>
         <meta name="theme-color" content="#ffffff"/>
 
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
