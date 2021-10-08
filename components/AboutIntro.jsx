eee383fcdde11839ce23f43b192dd48997f6cf09
shirleyxindi <s.x.li@student.tudelft.nl>
2021-10-08 14:10:54 +0200

    Adjusted opening times

--- components/AboutIntro.jsx
+++ components/AboutIntro.jsx
@@ -5,7 +5,7 @@ export const AboutIntro = () => {
     return (
         <section className={styles.container}>
           <h1>About the <span>/Pub</span></h1>
-          <p>The /Pub is a bar located in the basement of EEMCS, the Faculty of Electrical Engineering, Mathematics and Computer Science of Delft University of Technology. It is open every Wednesday and Thursday from 4pm to 7pm. The exact closing time depends on the bartenders on duty and any scheduled activities after closing.</p>
+          <p>The /Pub is a bar located in the basement of EEMCS, the Faculty of Electrical Engineering, Mathematics and Computer Science of Delft University of Technology. It is open every Wednesday and Thursday from 16:00 - 22:00. The exact closing time depends on the bartenders on duty and any scheduled activities after closing.</p>
           <p>Since 2019, the /Pub is being run by 6 students, the /Pub Board. This Board consists of 3 students from W.I.S.V Christiaan Huygens (CH) and 3 students from Elektrotechnische Vereeniging (ETV). You can view the current and older /Pub Board and founders in the <Link to="boards" spy={true} smooth={true} duration={1000}>Board section</Link>. It is a part time Board that makes sure that everything in the /Pub runs smoothly, such as the financial health, inventory, bartenders, big renovations etc. Next to that, they also organise events in the /Pub. You can view the events on the Events page. The /Pub Board is (almost) always somewhere in the /Pub, so when you are in the /Pub, don’t hesitate to chat with us!</p>
         </section>
     )
