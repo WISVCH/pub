2db91aa8be9987dd9053bccd3979aa46be28bc29
sxli <s.x.li@student.tudelft.nl>
2021-11-11 15:13:36 +0100

    Updated board 3 photo

--- components/Boards.jsx
+++ components/Boards.jsx
@@ -15,7 +15,7 @@ export const Boards = () => {
                 <Button onClick={() => setActiveBoard(0)} active={activeBoard == 0}>Founders</Button>
             </div>
             <div className={styles.boardWrapper}>
-                {activeBoard == 3 && <Board name="Board 3" img={"/images/board-3.jpeg"} chairman="Shirley Li " secretary="Amanda Krudde" treasurer="Lars van Tol" commissionerOfInventory="Simon Molenkamp" commissionerOfEvents="Bart Zuidema" commissionerOfMaintenance="Daan Schat " qualitateQua="Niels Mateijsen &amp; Maxim Maxurovs"/>}
+                {activeBoard == 3 && <Board name="Board 3" img={"/images/board-3.jpg"} chairman="Shirley Li " secretary="Amanda Krudde" treasurer="Lars van Tol" commissionerOfInventory="Simon Molenkamp" commissionerOfEvents="Bart Zuidema" commissionerOfMaintenance="Daan Schat " qualitateQua="Niels Mateijsen &amp; Maxim Maxurovs"/>}
                 {activeBoard == 2 && <Board name="Board 2" img={"/images/board-2.jpeg"} chairman="Owen van Hooff" secretary="Maxim Maxurovs" treasurer="Niels Mateijsen" commissionerOfInventory="Sam de Jong" commissionerOfEvents="Joep de Jong" commissionerOfMaintenance="Robbin Poll" qualitateQua="Frederiek Backers &amp; Joris van Breukelen"/>}
                 {activeBoard == 1 && <Board name="Board 1" img={"/images/board-1.jpg"} chairman="Bastiaan Bakker" secretary="Marcel Brouwers" treasurer="Frederiek Backers" commissionerIntern="Joris van Breukelen" commissionerOfInventory="Rona Roovers" commissionerOfBarkeepers="David van Beelen" qualitateQua="Louise Zwep &amp; Joos Vrijdag"/>}
                 {activeBoard == 0 && <Board name="Founders" img={"/images/board-0.jpeg"} chairman="Rik Westdorp" secretary="Philip Groet" treasurer="Willem de Laat" commissioner="Yanna van der Vlugt"/>}
