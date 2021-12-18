96cf75d3eb2d73eb31ac987efe832ebf69645759
sxli <s.x.li@student.tudelft.nl>
2021-12-18 16:33:18 +0100

    Updated closed message (closed until at least 15th of January)

--- components/ClosedMessage.jsx
+++ components/ClosedMessage.jsx
@@ -4,7 +4,7 @@ export const ClosedMessage = () => {
     return (
         <div className={styles.container}>
             <h1>Temporarily closed</h1>
-            <p>Due to corona, the Executive Board of the TU Delft has decided all faculty bars have to close. Therefore, the /Pub is unfortunately closed until at least the 19th of December.</p>
+            <p>Due to corona, the /Pub is unfortunately closed until at least the 15th of January. We hope to see you again in the new year!</p>
         </div>
     )
 }
