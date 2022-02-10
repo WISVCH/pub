3ce68371160e0f8e16d43cb5618bbf7901006754
sxli <s.x.li@student.tudelft.nl>
2022-02-10 18:00:45 +0100

    update capacity

--- components/CoronaMessage.jsx
+++ components/CoronaMessage.jsx
@@ -5,7 +5,7 @@ export const CoronaMessage = () => {
         <div className={styles.container}>
             <h1>We are open again!</h1>
             <p>We are very excited to announce that we will be open again from February 9th! However, due to corona, everyone has to stay seated and keep 1,5m distance.
-            There also is a maximum capacity of 75 people and masks are mandatory when walking. Can't wait to see you all again in the /Pub!
+            There also is a maximum capacity of 65 people and masks are mandatory when walking. Can't wait to see you all again in the /Pub!
             </p>
         </div>
     )
