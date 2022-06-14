8c45ee4d15f649116c69d66a204a2f46250a70ae
sxli <s.x.li@student.tudelft.nl>
2022-06-14 19:26:31 +0200

    closed during summer break message

--- components/CoronaMessage.jsx
+++ components/CoronaMessage.jsx
@@ -4,7 +4,7 @@ export const CoronaMessage = () => {
     return (
         <div className={styles.container}>
             <h1>Closed during holidays</h1>
-            <p>The /Pub will be closed on the following days due to national holidays: April 27th, May 4th, 5th and 26th.
+            <p>The /Pub will be closed during the summer break! The last 'borrel' will be on Friday 24th.
             </p>
         </div>
     )
